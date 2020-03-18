0e6a9b9af68adffa989549cdd5fbb4e49f33ac6f
Marco Petrini <marcopetrini1994@gmail.com>
2020-03-18 14:16:26 +0100

    creazione logica script

---
+++ main.js
@@ -0,0 +1,20 @@
+/*  obiettivo esercizio
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito19
+*******************/
+
+
+
+// creazione variabile per nome utente
+
+// creazione variabile per cognome utente
+
+// creazione variabile per colore preferito utente
+
+// assegnazione valore alla variabile nome utente
+
+// assegnazione valore variabile per cognome utente
+
+// assegnazione valore variabile per colore preferito utente
